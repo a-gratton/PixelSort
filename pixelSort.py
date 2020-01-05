40f8a4bd6492d1b79e29ade78cd248d716ac3759
Aidan Gratton <gratton.aidan@gmail.com>
2020-01-04 22:32:40 -0500

    Added option to ignore noise when defining sort ranges

--- pixelSort.py
+++ pixelSort.py
@@ -5,17 +5,28 @@ import multiprocessing as mp
 from functools import partial
 
 
-# Modes:
-#  brighter: Sort pixels brighter than the set black point.
-#            each range of non black pixels sorted separately
-#   darker: Sort pixels darker than the set white point
-#           each range of non white pixels sorted separately
+# Range Modes:
+#   brighter:
+#       Sort pixels brighter than the set black point.
+#           each range of non black pixels sorted separately.
+#   darker:
+#       Sort pixels darker than the set white point.
+#           each range of non white pixels sorted separately.
+#
+# Sort Modes:
+#   sort:
+#       The pixels in each range are sorted by the given parameter (HSL).
+#   average:
+#       the pixels in each range are averaged, then all pixels in the range are set to this value.
+#
+# Noise ignore parameter:
+#   sets the minimum length of each range; minimizes impact of image noise on range selection.
 
 
 def main():
-    mode = SortMode('col', 'l', 'brighter', 'average', 1, 30, 256)
+    mode = SortMode('row', 'l', 'darker', 'sort', -1, 50, 120, 150, 40)
     try:
-        im = Image.open("Writing On Stone Star Composite.jpg").convert("HSV")
+        im = Image.open("Candle lit (blue-purple).jpg").convert("HSV")
         print(im.format, im.size, im.mode)
         sort(im, mode)
         im.show()
@@ -28,27 +39,35 @@ def sort(im, mode):
     pixels = im.load()
     if mode.row:
         if mode.range_mode == 0 or mode.range_mode == 1:
-            srt = partial(sortRow, im, mode)
+            srt = partial(rowHandler, im, mode)
             workers = pool.map(srt, range(im.size[1]))
             for q in range(len(workers)):
                 rowInsert(pixels, workers[q][0], workers[q][1], mode, q)
     else:
         if mode.range_mode == 0 or mode.range_mode == 1:
-            srt = partial(sortCol, im, mode)
+            srt = partial(colHandler, im, mode)
             workers = pool.map(srt, range(im.size[0]))
             for q in range(len(workers)):
                 colInsert(pixels, workers[q][0], workers[q][1], mode, q)
 
 
-def sortCol(source, mode, i):
+def colHandler(source, mode, i):
     pixels = source.load()
+    sort_range = [0]
+    temp_range = []
+
     if mode.range_mode == 0:
-        sort_range = [0]
         for j in range(source.size[1]):
             if mode.black < pixels[i, j][2]:
-                sort_range.append(j)
-            elif sort_range[-1] != -1:
-                sort_range.append(-1)
+                temp_range.append(j)
+            else:
+                if sort_range[-1] != -1:
+                    sort_range.append(-1)
+                if len(temp_range) > mode.ignore_noise:
+                    sort_range += temp_range
+                temp_range = []
+        else:
+            sort_range += temp_range
         col = [(0, 0, 0)]
         cols = [[(0, 0, 0), ], ]
         for j in sort_range:
@@ -59,22 +78,29 @@ def sortCol(source, mode, i):
                 cols += [[(0, 0, 0)]]
                 col = [(0, 0, 0)]
         cols[-1] = col
+
         if mode.sort_mode == 0:
             for subset in cols:
                 radixSort(subset, 255, mode.hsl)
             return [cols, sort_range]
+
         elif mode.sort_mode == 1:
             for subset in cols:
-                averageRange(subset)
+                averageRange(subset, mode)
             return [cols, sort_range]
 
     elif mode.range_mode == 1:
-        sort_range = [0]
         for j in range(source.size[1]):
             if pixels[i, j][2] < mode.white:
-                sort_range.append(j)
-            elif sort_range[-1] != -1:
-                sort_range.append(-1)
+                temp_range.append(j)
+            else:
+                if sort_range[-1] != -1:
+                    sort_range.append(-1)
+                if len(temp_range) > mode.ignore_noise:
+                    sort_range += temp_range
+                temp_range = []
+        else:
+            sort_range += temp_range
         col = [(0, 0, 0)]
         cols = [[(0, 0, 0), ], ]
         for j in sort_range:
@@ -86,25 +112,34 @@ def sortCol(source, mode, i):
                 col = [(0, 0, 0)]
         else:
             cols[-1] = col
+
         if mode.sort_mode == 0:
             for subset in cols:
                 radixSort(subset, 255, mode.hsl)
             return [cols, sort_range]
+
         elif mode.sort_mode == 1:
             for subset in cols:
-                averageRange(subset)
+                averageRange(subset, mode)
             return [cols, sort_range]
 
 
-def sortRow(source, mode, i):
+def rowHandler(source, mode, i):
     pixels = source.load()
+    sort_range = [0]
+    temp_range = []
     if mode.range_mode == 0:
-        sort_range = [0]
         for j in range(source.size[0]):
             if mode.black < pixels[j, i][2]:
-                sort_range.append(j)
-            elif sort_range[-1] != -1:
-                sort_range.append(-1)
+                temp_range.append(j)
+            else:
+                if sort_range[-1] != -1:
+                    sort_range.append(-1)
+                if len(temp_range) > mode.ignore_noise:
+                    sort_range += temp_range
+                temp_range = []
+        else:
+            sort_range += temp_range
         row = [(0, 0, 0)]
         rows = [[(0, 0, 0), ], ]
         for j in sort_range:
@@ -115,22 +150,29 @@ def sortRow(source, mode, i):
                 rows += [[(0, 0, 0)]]
                 row = [(0, 0, 0)]
         rows[-1] = row
+
         if mode.sort_mode == 0:
             for subset in rows:
                 radixSort(subset, 255, mode.hsl)
             return [rows, sort_range]
+
         elif mode.sort_mode == 1:
             for subset in rows:
-                averageRange(subset)
+                averageRange(subset, mode)
             return [rows, sort_range]
 
     elif mode.range_mode == 1:
-        sort_range = [0]
         for j in range(source.size[0]):
             if pixels[j, i][2] < mode.white:
-                sort_range.append(j)
-            elif sort_range[-1] != -1:
-                sort_range.append(-1)
+                temp_range.append(j)
+            else:
+                if sort_range[-1] != -1:
+                    sort_range.append(-1)
+                if len(temp_range) > mode.ignore_noise:
+                    sort_range += temp_range
+                temp_range = []
+        else:
+            sort_range += temp_range
         row = [(0, 0, 0)]
         rows = [[(0, 0, 0), ], ]
         for j in sort_range:
@@ -142,26 +184,16 @@ def sortRow(source, mode, i):
                 rows += [[(0, 0, 0)]]
                 row = [(0, 0, 0)]
         rows[-1] = row
+
         if mode.sort_mode == 0:
             for subset in rows:
                 radixSort(subset, 255, mode.hsl)
             return [rows, sort_range]
+
         elif mode.sort_mode == 1:
             for subset in rows:
-                averageRange(subset)
+                averageRange(subset, mode)
             return [rows, sort_range]
-
-
-def averageRange(source):
-    average = [0, 0, 0]
-    for pixel in source:
-        for i in range(3):
-            average[i] += pixel[i]
-    for i in range(3):
-        average[i] //= len(source)
-    newPixel = (average[0], average[1], average[2])
-    for i in range(len(source)):
-        source[i] = newPixel
 
 
 def colInsert(pixels, cols, sort_range, mode, col):
@@ -184,6 +216,18 @@ def rowInsert(pixels, rows, sort_range, mode, row):
         else:
             curr += 1
             index = 0
+
+
+def averageRange(source, mode):
+    average = [0, 0, 0]
+    for pixel in source:
+        for i in range(3):
+            average[i] += pixel[i]
+    for i in range(3):
+        average[i] //= len(source)
+    newPixel = (average[0], average[1], average[2] + mode.lum_bias)
+    for i in range(len(source)):
+        source[i] = newPixel
 
 
 def radixSort(source, key, hsl):
