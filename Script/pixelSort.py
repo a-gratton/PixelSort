83cb7fa0daeb07be5bda786896f02b35d069d7b1
Aidan Gratton <gratton.aidan@gmail.com>
2020-02-11 00:19:03 -0500

    Updated comments

--- Script/pixelSort.py
+++ Script/pixelSort.py
@@ -8,22 +8,25 @@ from functools import partial
 # Range Modes:
 #   brighter:
 #       Sort pixels brighter than the set black point.
-#           each range of non black pixels sorted separately.
+#           Each range of non black pixels sorted separately.
 #   darker:
 #       Sort pixels darker than the set white point.
-#           each range of non white pixels sorted separately.
+#           Each range of non white pixels sorted separately.
 #
 # Sort Modes:
 #   sort:
 #       The pixels in each range are sorted by the given parameter (HSL).
 #   average:
-#       the pixels in each range are averaged, then all pixels in the range are set to this value.
+#       The pixels in each range are averaged, then all pixels in the range are set to this value.
+#       Luminance values are increased by the luminance bias parameter value.
 #
 # Noise ignore parameter:
-#   sets the minimum length of each range; minimizes impact of image noise on range selection.
+#   Sets the minimum length of each range; minimizes impact of image noise on range selection.
 
 
 def main():
+    # parameters: row/col, hsl mode, range selection mode, sort mode, direction,
+    #             noise parameter, black point, white point, average mode luminance bias
     mode = SortMode('row', 'l', 'brighter', 'sort', -1, 250, 20, 150, 40)
     try:
         im = Image.open("./Example Images/Mixed Signals.jpg").convert("HSV")
@@ -32,46 +35,56 @@ def main():
         im = im.convert("RGB")
         im.save('Mixed Signals-edited.png')
 
+    # if file is not found:
     except IOError:
         print("File not found")
 
 
 def sort(im, mode):
-    pool = mp.Pool(processes=4)
-    pixels = im.load()
+    # distributes rows/columns between worker processes
+
+    pool = mp.Pool(processes=4)  # setting up worker pool (set equal to number of CPU cores)
+    pixels = im.load()  # loading pixel map of image
+
+    # sorting rows
     if mode.row:
-        if mode.range_mode == 0 or mode.range_mode == 1:
-            srt = partial(rowHandler, im, mode)
+        if mode.range_mode == 0 or mode.range_mode == 1:  # extraneous if statement to cover future expansion
+            srt = partial(rowHandler, im, mode)  # using partial for easier use of pool.map
             workers = pool.map(srt, range(im.size[1]))
             for q in range(len(workers)):
-                rowInsert(pixels, workers[q][0], workers[q][1], mode, q)
+                rowInsert(pixels, workers[q][0], workers[q][1], mode, q)  # calls row insert function, then frees worker
+
+    # sorting columns
     else:
-        if mode.range_mode == 0 or mode.range_mode == 1:
-            srt = partial(colHandler, im, mode)
+        if mode.range_mode == 0 or mode.range_mode == 1:  # extraneous if statement to cover future expansion
+            srt = partial(colHandler, im, mode)  # using partial for easier use of pool.map
             workers = pool.map(srt, range(im.size[0]))
             for q in range(len(workers)):
-                colInsert(pixels, workers[q][0], workers[q][1], mode, q)
+                colInsert(pixels, workers[q][0], workers[q][1], mode, q)  # calls col insert function, then frees worker
 
 
 def colHandler(source, mode, i):
-    pixels = source.load()
+    # function to handle column sorting
+    pixels = source.load()  # loading pixel map of image
     sort_range = [0]
     temp_range = []
 
-    if mode.range_mode == 0:
+    if mode.range_mode == 0:  # "brighter" selection mode
         for j in range(source.size[1]):
             if mode.black < pixels[i, j][2]:
-                temp_range.append(j)
+                temp_range.append(j)  # building temporary sort range list
             else:
                 if sort_range[-1] != -1:
-                    sort_range.append(-1)
-                if len(temp_range) > mode.ignore_noise:
-                    sort_range += temp_range
-                temp_range = []
+                    sort_range.append(-1)  # signifies break in sort range (edge)
+                if len(temp_range) > mode.ignore_noise:  # if temp sort range is longer than noise parameter:
+                    sort_range += temp_range  # append temp range to sort range list
+                temp_range = []  # reset temp range
         else:
-            sort_range += temp_range
+            sort_range += temp_range  # appending last temp range if it reaches the end of the image
         col = [(0, 0, 0)]
         cols = [[(0, 0, 0), ], ]
+
+        # breaking up sort range list into 2D list
         for j in sort_range:
             if j != -1:
                 col.append(pixels[i, j])
@@ -81,30 +94,32 @@ def colHandler(source, mode, i):
                 col = [(0, 0, 0)]
         cols[-1] = col
 
-        if mode.sort_mode == 0:
+        if mode.sort_mode == 0:  # if mode is "sort"
             for subset in cols:
-                radixSort(subset, 255, mode.hsl)
-            return [cols, sort_range]
+                radixSort(subset, 255, mode.hsl)  # sort ranges using radix base-16 sort
+            return [cols, sort_range]  # return parameters to col insert function
 
-        elif mode.sort_mode == 1:
+        elif mode.sort_mode == 1:  # if mode is "average"
             for subset in cols:
-                averageRange(subset, mode)
-            return [cols, sort_range]
+                averageRange(subset, mode)  # average pixel values in each range
+            return [cols, sort_range]  # return parameters to col insert function
 
-    elif mode.range_mode == 1:
+    elif mode.range_mode == 1:  # "darker" selection mode
         for j in range(source.size[1]):
             if pixels[i, j][2] < mode.white:
-                temp_range.append(j)
+                temp_range.append(j)  # building temporary sort range list
             else:
                 if sort_range[-1] != -1:
-                    sort_range.append(-1)
-                if len(temp_range) > mode.ignore_noise:
-                    sort_range += temp_range
-                temp_range = []
+                    sort_range.append(-1)  # signifies break in sort range (edge)
+                if len(temp_range) > mode.ignore_noise:  # if temp sort range is longer than noise parameter:
+                    sort_range += temp_range  # append temp range to sort range list
+                temp_range = []  # reset temp range
         else:
-            sort_range += temp_range
+            sort_range += temp_range  # appending last temp range if it reaches the end of the image
         col = [(0, 0, 0)]
         cols = [[(0, 0, 0), ], ]
+
+        # breaking up sort range list into 2D list
         for j in sort_range:
             if j != -1:
                 col.append(pixels[i, j])
@@ -115,18 +130,19 @@ def colHandler(source, mode, i):
         else:
             cols[-1] = col
 
-        if mode.sort_mode == 0:
+        if mode.sort_mode == 0:  # if mode is "sort"
             for subset in cols:
-                radixSort(subset, 255, mode.hsl)
-            return [cols, sort_range]
+                radixSort(subset, 255, mode.hsl)  # sort ranges using radix base-16 sort
+            return [cols, sort_range]  # return parameters to col insert function
 
-        elif mode.sort_mode == 1:
+        elif mode.sort_mode == 1:  # if mode is "average"
             for subset in cols:
-                averageRange(subset, mode)
-            return [cols, sort_range]
+                averageRange(subset, mode)  # average pixel values in each range
+            return [cols, sort_range]  # return parameters to col insert function
 
 
 def rowHandler(source, mode, i):
+    # the same structure as colHandler, but for rows
     pixels = source.load()
     sort_range = [0]
     temp_range = []
@@ -198,7 +214,7 @@ def rowHandler(source, mode, i):
             return [rows, sort_range]
 
 
-def colInsert(pixels, cols, sort_range, mode, col):
+def colInsert(pixels, cols, sort_range, mode, col):  # replaces sorted columns of original image
     index = curr = 0
     for j in sort_range:
         if not (j == -1):
@@ -210,6 +226,7 @@ def colInsert(pixels, cols, sort_range, mode, col):
 
 
 def rowInsert(pixels, rows, sort_range, mode, row):
+    # replaces sorted rows of original image
     index = curr = 0
     for j in sort_range:
         if not (j == -1):
@@ -221,18 +238,21 @@ def rowInsert(pixels, rows, sort_range, mode, row):
 
 
 def averageRange(source, mode):
+    # averages the pixel values in each range
     average = [0, 0, 0]
     for pixel in source:
         for i in range(3):
             average[i] += pixel[i]
     for i in range(3):
         average[i] //= len(source)
-    newPixel = (average[0], average[1], average[2] + mode.lum_bias)
+    newPixel = (average[0], average[1], average[2] + mode.lum_bias)  # adds luminance bias parameter (brightens ranges)
     for i in range(len(source)):
         source[i] = newPixel
 
 
 def radixSort(source, key, hsl):
+    # radix base-16 sort (using bitwise operators)
+    # note that pixel[hsl] accesses the hue, saturation, or luminance as specified in original parameters
     pval = 0
     while key >> pval > 0:
         countingSort(source, pval, hsl)
@@ -240,11 +260,12 @@ def radixSort(source, key, hsl):
 
 
 def countingSort(source, pval, hsl):
-    elementCount = [0] * 16
+    # subroutine for radix sort
+    elementCount = [0] * 16  # digit count list
     ordered = [(0, 0, 0)] * len(source)
     for pixel in source:
-        if pixel[2] >= 0:
-            elementCount[(pixel[hsl] >> pval) & 0xF] += 1
+        if pixel[2] >= 0:  # handling edge case of 0 brightness pixels
+            elementCount[(pixel[hsl] >> pval) & 0xF] += 1  # selecting base 16 digit with bitwise operators
         else:
             elementCount[0] += 1
     for i in range(1, 16):
